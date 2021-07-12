4e370815cfdcf0cf259d43f537969b2b42de623a
kh <gongkehong626@gmail.com>
2021-07-12 23:35:44 +0800

    update eval

--- run_evaluate.py
+++ run_evaluate.py
@@ -30,7 +30,10 @@ def main(args):
     assert path.isfile(args.evaluate), '==> No checkpoint found at {}'.format(args.evaluate)
     print("==> Loading checkpoint '{}'".format(args.evaluate))
     ckpt = torch.load(args.evaluate)
-    model_pos.load_state_dict(ckpt['state_dict'])
+    try:
+        model_pos.load_state_dict(ckpt['state_dict'])
+    except:
+        model_pos.load_state_dict(ckpt['model_pos'])
 
     print('==> Evaluating...')
 
@@ -38,7 +41,7 @@ def main(args):
     print('H36M: Protocol #1   (MPJPE) overall average: {:.2f} (mm)'.format(error_h36m_p1))
     print('H36M: Protocol #2 (P-MPJPE) overall average: {:.2f} (mm)'.format(error_h36m_p2))
 
-    error_3dhp_p1, error_3dhp_p2 = evaluate(data_dict['3DHP_test'], model_pos, device)
+    error_3dhp_p1, error_3dhp_p2 = evaluate(data_dict['3DHP_test'], model_pos, device, flipaug='_flip')
     print('3DHP: Protocol #1   (MPJPE) overall average: {:.2f} (mm)'.format(error_3dhp_p1))
     print('3DHP: Protocol #2 (P-MPJPE) overall average: {:.2f} (mm)'.format(error_3dhp_p2))
 
